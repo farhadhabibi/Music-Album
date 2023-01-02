3b222d215218a6eb5428b2510dd6d15373090f3c
farhad-habibi <farhadmalekzada@gmail.com>
2023-01-02 16:57:17 +0500

    fetched data from Itunes API

--- src/Music.js
+++ src/Music.js
@@ -1,19 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-const API_BASE_URL = 'https://itunes.apple.com/us/rss/topalbums/limit=4/json';
+const API_BASE_URL = 'https://itunes.apple.com/us/rss/topalbums/limit=12/json';
 
 function Music() {
+    const [music, setMusic] = useState();
+
+    useEffect(() => {
+        async function fetchMusic() {
+            const response = await axios.get(API_BASE_URL)
+            console.log('data', response.data.feed.entry);
+            setMusic(response.data.feed)
+        }
+        fetchMusic();
+    }, [])
     return (
-        < div className="p-5 rounded-lg shadow-xl bg-neutral-700 max-w-sm col-span-2 m-2" >
-            {/* {< img class="w-full" src="/img/card-top.jpg" alt="Music photo" />} */}
-            <h5 className="text-white text-xl leading-tight font-medium mb-2">Card title</h5>
-            <p className="text-white text-base mb-4">
-                Some quick example text to build on the card title and make up the bulk of the card's
-                content.
-            </p>
-            <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Button</button>
-        </div >
+        <>
+            {
+                music && music.entry.length > 0 && music.entry.map((music, index) => {
+                    return (
+                        <div key={music.id.attributes['im:id']} className="rounded-lg shadow-xl bg-neutral-600 max-w-sm col-span-2 m-2">
+                            <img className="rounded-sm w-full mb-1" src={music['im:image'][2].label} alt="Music photo" />
+                            <div className="ml-2">
+                                <h5 className="text-slate-100 text-base leading-tight font-medium mb-2">{music['im:name'].label}</h5>
+                                <p className="text-slate-300 text-sm mb-4">{music['im:artist'].label}</p>
+                                {/* <p className="text-white text-base mb-4">{music['im:releaseDate'].attributes.label}</p>
+                                <p className="text-white text-base mb-4">{music['im:price'].label}</p> */}
+                            </div>
+                            {/* <button type="button" className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight 
+                            uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none 
+                            focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out w-full">Contact Us</button> */}
+                        </div>
+                    )
+                })
+            }
+        </>
     )
 }
 export default Music;
-
--- src/MusicAlbum.js
+++ src/MusicAlbum.js
@@ -3,19 +3,14 @@ import React from 'react';
 import Music from './Music';
 
 function MusicAlbum() {
-    const data = Array(12).fill();
     return (
         <div className="grid grid-cols-8 bg-neutral-700 col-start-3 gap-3 col-span-8 mt-20 h-4/6 overflow-auto">
             <div className="flex justify-between bg-neutral-900 py-1 items-center col-span-full text-white">
-                <p className="ml-3"> Top 100 musics </p>
+                <p className="ml-3"> Top 100 Albums </p>
                 <input className="mr-3 w-72 placeholder:italic placeholder:text-slate-100 block bg-neutral-700 rounded-md py-2 pl-9 pr-10 shadow-sm focus:outline-none sm:text-sm" placeholder="Search for music..." type="text" name="search" />
             </div>
             {
-                data.map((item, index) => {
-                    return (
-                        <Music key={index} />
-                    )
-                })
+                <Music />
             }
         </div>
     )
